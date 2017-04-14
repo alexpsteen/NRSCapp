3937b6cc0c81cc4ad976cddbfbf7719830408586
ericachia <ercachia@gmail.com>
2017-04-14 17:31:59 -0400

    added events to database

--- app/src/main/java/com/example/alexsteen/nrscapp/EventMain.java
+++ app/src/main/java/com/example/alexsteen/nrscapp/EventMain.java
@@ -1,6 +1,7 @@
 package com.example.alexsteen.nrscapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,8 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class EventMain extends AppCompatActivity {
+
+    private SharedPreferences currentEvent;
+    private eventDB dbHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +25,22 @@ public class EventMain extends AppCompatActivity {
         setContentView(R.layout.activity_event_main);
         getSupportActionBar().hide();
 
+        currentEvent = getSharedPreferences("CurrentEvent", MODE_PRIVATE);
+        dbHandler = new eventDB(this);
+
         ImageView btn_back = (ImageView) findViewById(R.id.btn_back);
         Button add_feature = (Button) findViewById(R.id.addFeature);
+        TextView event_name = (TextView) findViewById(R.id.tb_eventName);
+        TextView date_range = (TextView) findViewById(R.id.tb_dateRange);
+
+        String eventName = currentEvent.getString("eventName",null);
+        event_name.setText(eventName);
+        String startDate = dbHandler.getStartDate(eventName);
+        String endDate = dbHandler.getEndDate(eventName);
+        String dateRange = startDate + " - " + endDate;
+        date_range.setText(dateRange);
+
+
 
         TabHost tabHost = (TabHost)findViewById(R.id.tabhost);
 //        tabHost.setup();
